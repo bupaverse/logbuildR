efd83ad52fdb231e815596c2e40edbb7aabb2c5a
gertjanssenswillen <gert.janssenswillen@uhasselt.be>
2023-03-02 11:29:37 +0100

    fix script

--- R/compile_script.R
+++ R/compile_script.R
@@ -6,31 +6,33 @@ compile_script <- function(CO, constructor) {
 
 
 if(!is.null(CO$timestamps_to_prepare))  {
-   timestamp_conversion <- glue::glue("convert_timestamps(columns = c(\"{paste(CO$timestamps_to_prepare, collapse = '\",\"')}\"), format = {CO$timestamps_format}) %>%")
+   timestamp_conversion <- glue::glue("convert_timestamps(columns = c(\"{paste(CO$timestamps_to_prepare, collapse = '\",\"')}\"), format = {CO$timestamps_format}) %>%\n")
 } else {
     timestamp_conversion <- ""
 }
 if(!is.null(CO$lifecycle_to_recode)) {
-    lifecycle_conversion <- paste0("rename(", paste0(paste(CO$lifecycle_recode_to, CO$lifecycle_to_recode, sep = " = "), collapse = ", "), ") %>%")
+    lifecycle_conversion <- paste0("rename(", paste0(paste(CO$lifecycle_recode_to, CO$lifecycle_to_recode, sep = " = "), collapse = ", "), ") %>%\n")
 } else {
     lifecycle_conversion <- ""
 }
-    if(CO$guess_activity_instance_id) {
+    if(!is.null(CO$guess_activity_instance_id)) {
         activity_instance <- glue::glue("logbuildR::assign_instance_id(   '{CO$case_id}',\n\t          '{CO$activity_id}',
                                                      \t          '{CO$timestamps}',
-                                                     \t          '{CO$lifecycle_id}') %>%")
+                                                     \t          '{CO$lifecycle_id}') %>%\n")
     } else {
         activity_instance <- ""
     }
 
-if(CO$complete_lifecycle_added) {
-    lifecycle_add <- "mutate(lifecycle_logbuildR = 'complete') %>%"
+if(!is.null(CO$complete_lifecycle_added)) {
+    lifecycle_add <- "mutate(lifecycle_logbuildR = 'complete') %>%\n"
+} else {
+    lifecycle_add <- ""
 }
 
 glue::glue("{CO$data_name} %>%") -> data_selection
 
 
-paste(data_selection, timestamp_conversion, lifecycle_conversion, lifecycle_add, activity_instance, constructor , sep = "\n\t")
+paste(data_selection, timestamp_conversion, lifecycle_conversion, lifecycle_add, activity_instance, constructor , sep = "\t")
 
 }
 
